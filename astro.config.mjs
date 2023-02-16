6fb7f63deb8fea5fc6b79c9a00570403c732ce00
hideh1231 <hinagamura@gmail.com>
2023-02-16 17:39:15 +0900

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
